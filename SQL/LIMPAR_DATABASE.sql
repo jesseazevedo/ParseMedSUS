44947d36d1db4e4a8ae0bc38e5d9eebdfffe522d
Jessé Azevêdo <jessedanobrega@gmail.com>
2024-01-20 06:54:53 -0300

    first commit

---
+++ SQL/LIMPAR_DATABASE.sql
@@ -0,0 +1,9 @@
+delete from dbmobile.rl_medsus_refbibliog_drogacam;
+delete from dbmobile.rl_medsus_droga_campo;
+delete from dbmobile.rl_medsus_dropga_categoria;
+delete from dbmobile.tb_medsus_campo;
+delete from dbmobile.tb_medsus_ref_bibliografica;
+delete from dbmobile.tb_medsus_link_noticia;
+delete from dbmobile.tb_medsus_droga;
+delete from dbmobile.tb_medsus_categoria;
+
